7b051162ecb033816e010885962dd37ec0325ea6
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-09-06 08:33:16 +0500

    Initial Commit

---
+++ now/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  now
+//
+//  Created by Macbook Pro 14 Inch on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
